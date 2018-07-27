63951cf4d8996208e0695e811b171c8801c7834c
timpasternak <pasternak@nextbike.com>
2018-07-27 11:41:11 +0200

    Initial Setup for PTS2

---
+++ app/Http/Controllers/API/Usermanagement/PublicUserManagement.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: timpasternak
+ * Date: 27.07.18
+ * Time: 11:36
+ */
+
+namespace App\Http\Controllers\API\Usermanagement;
+
+
+use App\Http\Controllers\Controller;
+
+class PublicUserManagement extends Controller
+{
+
+
+
+}
